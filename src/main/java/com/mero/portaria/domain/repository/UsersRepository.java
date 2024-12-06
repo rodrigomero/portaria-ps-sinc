120dd71696a34e5d58519137a82227700debf728
rodrigomero <meroo.rodrigo@gmail.com>
2024-12-06 02:00:13 -0300

    :fire: Removed outdated files

--- src/main/java/com/mero/portaria/domain/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mero.portaria.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-}
